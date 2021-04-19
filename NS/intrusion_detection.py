806b3ec7860b91ea469fe135e1fd8e6e90512f36
Julius Alphonso <juliusalphonso.09@gmail.com>
2021-04-19 11:23:19 +0530

    NS: Warn if file does not exist

--- NS/intrusion_detection.py
+++ NS/intrusion_detection.py
@@ -2,12 +2,17 @@ from scipy import stats
 import numpy as np
 import pandas as pd
 import sys
+import os
 
 if len(sys.argv) < 2:
     print("USAGE: python intrusion_detection.py dataset_file [threshold]\nDataset is a newline separated list of numbers\nthreshold is the positive z-score threshold value [default=1]")
     exit(42)
 
 threshold = len(sys.argv) == 3 and x[2] or 1
+
+if not os.path.isfile(sys.argv[1]):
+    print(f"File {sys.argv[1]} does not exist")
+    exit(6)
 
 try:
     data = open(sys.argv[1])
@@ -17,6 +22,7 @@ try:
     data = np.array(data)
 except:
     print("Error: Could not read and format data")
+    exit(9)
 
 z_data = stats.zscore(data)
 abnormal = np.abs(z_data) > threshold
